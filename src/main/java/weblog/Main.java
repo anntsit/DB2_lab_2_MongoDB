aecc3690585d75fc937686414281945c0f5ed69d
anntsit <anyatsit98@ukr.net>
2017-12-07 16:25:05 +0200

    add functionality

--- src/main/java/weblog/Main.java
+++ src/main/java/weblog/Main.java
@@ -1,8 +1,25 @@
 package weblog;
 
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        MongoDBConnection connection = new MongoDBConnection();
-        connection.printCollection();
+        MongoDBConnection connection = new MongoDBConnection("weblogs", "logs");
+        printQuery(connection.getAll());
+        //printQuery(connection.findSortedIPbyURL("https://ru.wikipedia.org"));
+        //printQuery(connection.findSortedURLbuIP("192.0.2.235"));
+        //printQuery(connection.findSortedURLbuTimestamp("2017-11-05T22:15:00Z","2017-11-06T22:17:00Z"));
+        //printQuery(connection.findTotalDurationOfURL());
+        //printQuery(connection.findTimesOfVisitesToURL());
+        printQuery(connection.findIPSumAndDuration());
+    }
+
+    public static void printQuery(Iterator<Document> query){
+        while (query.hasNext()) {
+            System.out.println(query.next());
+        }
     }
 }
--- src/main/java/weblog/MongoDBConnection.java
+++ src/main/java/weblog/MongoDBConnection.java
@@ -4,19 +4,23 @@ import com.mongodb.*;
 import com.mongodb.client.*;
 import org.bson.Document;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.sql.Timestamp;
+import java.text.*;
+import java.util.*;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Indexes.descending;
 
 public class MongoDBConnection {
     private MongoClient mongo;
     private MongoDatabase database;
     private MongoCollection<Document> collection;
 
-    public void MongoConnection(String databaseName, String collectionName){
+    public MongoDBConnection(String databaseName, String collectionName){
         mongo = new MongoClient("localhost", 27017);
         database = mongo.getDatabase(databaseName);
         collection = database.getCollection(collectionName);
-        System.out.println("connected");
+        System.out.println("CONNECTED");
     }
 
     public void insertToCollection(ArrayList<Weblog> weblogs){
@@ -32,12 +36,93 @@ public class MongoDBConnection {
         collection.insertMany(documents);
     }
 
-    public void printCollection(){
-        MongoConnection("weblogs", "logs");
-        Iterator<Document> query = collection.find().iterator();
-        while(query.hasNext()) {
-            System.out.println(query.next());
-        }
-        mongo.close();
+    public Iterator<Document>  getAll(){
+        return collection.find().iterator();
+    }
+
+    public Iterator<Document> findSortedIPbyURL(String url){
+        return collection.find(new BasicDBObject("url", url)).sort(descending("IP")).iterator();
+    }
+
+    public Iterator<Document> findSortedURLbuIP(String ip){
+        return collection.find(new BasicDBObject("ip", ip)).sort(descending("URL")).iterator();
+    }
+
+    public Iterator<Document> findSortedURLbuTimestamp(String startTime, String endTime){
+        try{
+        Date dateFromFormated = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(startTime);
+        Date dateToFormated = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(endTime);
+        Timestamp timeStampFrom = new Timestamp((dateFromFormated.getTime()));
+        Timestamp timestampTo = new Timestamp((dateToFormated.getTime()));
+            return collection.find(and(gt("timeStamp", timeStampFrom),
+                    lt("timeStamp", timestampTo))).sort(descending("URL")).iterator();
+    }
+    catch (ParseException e) {
+        System.out.println("Problem occurred while parsing date");
+        return null;
+    }
+    }
+
+    public Iterator<Document> findTotalDurationOfURL(){
+        MongoCollection<Document> urlVisitDurationCollection =
+                database.getCollection("url_visit_duration");
+        String map = "function()" +
+                "{ emit(this.URL,this.timeSpent); }";
+        String reduce = "function(key, values)" +
+                "{return Array.sum(values)}";
+
+        collection.mapReduce(map, reduce).collectionName("url_visit_duration").toCollection();
+        Iterator<Document> webLogs = urlVisitDurationCollection.find()
+                .sort(descending("values")).iterator();
+        return webLogs;
+    }
+
+    public Iterator<Document> findTimesOfVisitesToURL(){
+        MongoCollection<Document> urlVisitSumCollection =
+                database.getCollection("url_visit_sum");
+        String map = "function()" +
+                "{ emit(this.URL,1); }";
+        String reduce = "function(key, values)" +
+                "{var count = 0; for(var i in values)" +
+                "{count += values[i];} " +
+                "return count;}";
+        collection.mapReduce(map, reduce).collectionName("url_visit_sum").toCollection();
+        Iterator<Document> webLogs = urlVisitSumCollection.find()
+                .sort(descending("value")).iterator();
+        return webLogs;
+    }
+
+    public Iterator<Document> findURLVisitsPerPeriod(String dateFrom, String dateTo) {
+        MongoCollection<Document> urlVisitPerPeriodCollection =
+                database.getCollection("url_visit_per_period");
+        String map = "function () {"
+                + "if (this.timeStamp > ISODate"
+                + "(\"" + dateFrom + "\")"
+                + " && this.timeStamp < ISODate"
+                + "(\"" + dateTo + "\"))"
+                + " emit(this.url, 1); }";
+        String reduce = "function(key, values) { return Array.sum(values); }";
+        collection.mapReduce(map, reduce).collectionName("url_visit_per_period").toCollection();
+        Iterator<Document> webLogs = urlVisitPerPeriodCollection.find()
+                .sort(descending("value")).iterator();
+        return webLogs;
+    }
+
+    public Iterator<Document> findIPSumAndDuration() {
+        MongoCollection<Document> ipVisitSumDurationCollection =
+                database.getCollection("ip_visit_sum_duration");
+        String map = "function(){" +
+                "values ={count:1,duration:this.timeSpent};" +
+                "emit(this.IP,values);}";
+        String reduce = "function(key, values) {" +
+                "var count=0; var duration=0;" +
+                "for(var i in values)" +
+                "{count += values[i].count;" +
+                "duration += values[i].duration;}" +
+                "return {count:count, duration:duration}}";
+        collection.mapReduce(map, reduce).collectionName("ip_visit_sum_duration").toCollection();
+        Iterator<Document> webLogs = ipVisitSumDurationCollection.find()
+                .sort(descending("count", "duration")).iterator();
+        return webLogs;
     }
 }
